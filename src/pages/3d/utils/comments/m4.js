f105dfd1c6d76d26ec3e6dd49b6cf5c9b58320e2
姚观寿 <yaoguanshou@rainbowcn.com>
2023-12-14 17:55:24 +0800

    透视投影添加

--- src/pages/3d/utils/comments/m4.js
+++ src/pages/3d/utils/comments/m4.js
@@ -22,6 +22,7 @@
       
 export default {
     
+  // 相机
     lookAt: function(cameraPosition, target, up) {
       var zAxis = normalize(
           subtractVectors(cameraPosition, target));
@@ -39,6 +40,65 @@ export default {
       ];
     },
   
+    /**
+     * Generates a perspective projection matrix with the given bounds.
+     * The near/far clip planes correspond to a normalized device coordinate Z range of [-1, 1],
+     * which matches WebGL/OpenGL's clip volume.
+     * Passing null/undefined/no value for far will generate infinite projection matrix.
+     *
+     * @param {mat4} out mat4 frustum matrix will be written into
+     * @param {number} fovy Vertical field of view in radians
+     * @param {number} aspect Aspect ratio. typically viewport width/height
+     * @param {number} near Near bound of the frustum
+     * @param {number} far Far bound of the frustum, can be null or Infinity
+     * @returns {mat4} out
+     */
+
+    
+    perspectiveNO:function(out, fovy, aspect, near, far) {
+
+      var f = 1.0 / Math.tan(fovy / 2),
+          nf;
+      out[0] = f / aspect;
+      out[1] = 0;
+      out[2] = 0;
+      out[3] = 0;
+      out[4] = 0;
+      out[5] = f;
+      out[6] = 0;
+      out[7] = 0;
+      out[8] = 0;
+      out[9] = 0;
+      out[11] = -1;
+      out[12] = 0;
+      out[13] = 0;
+      out[15] = 0;
+
+      if (far != null && far !== Infinity) {
+        nf = 1 / (near - far);
+        out[10] = (far + near) * nf;
+        out[14] = 2 * far * near * nf;
+      } else {
+        out[10] = -1;
+        out[14] = -2 * near;
+      }
+
+      /*
+        [
+          f / aspect,  0, 0,                    0,
+            0,          f, 0,                    0,
+            0,          0, (far + near) * nf,   -1,
+            0,          0, 2 * far * near * nf,  0,  
+        ]
+      
+      
+      */
+
+      
+      return out;
+    },
+
+    // 透视投影
     perspective: function(fieldOfViewInRadians, aspect, near, far) {
       var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
       var rangeInv = 1.0 / (near - far);
