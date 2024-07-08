e33e16b41acc910cfddf2d20ac270f2b12ff2322
姚观寿 <yaoguanshou@rainbowcn.com>
2024-07-08 20:25:39 +0800

    webgl学习

--- src/pages/3d/webgl/webgl编程指南/10高级技术/07绘制圆形点RoundedPoints/index.js
+++ src/pages/3d/webgl/webgl编程指南/10高级技术/07绘制圆形点RoundedPoints/index.js
@@ -16,37 +16,27 @@ import "./index.less";
 // import "@/pages/index.less";
 window.onload = function () {
 
- 
-
- 
-
- 
   const canvas = document.createElement("canvas");
   canvas.width = 500;
   canvas.height = 500;
   canvas.classList.add('canvas_webgl'); 
   document.body.appendChild(canvas);
 
-
- 
   if (!canvas.getContext) return;
   // alpha: false
   let gl = canvas.getContext("webgl", {});
   // vertexShader, fragmentShader
-
+   // 导入 shader
   const program = initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
   if (!program) {
     console.log("failed to initialize shaders");
     return;
   }
 
- 
-
   function main() {
-   
- 
   
     // Set the vertex information
+    // 初始化顶点
     var n = initVertexBuffers(gl);
     if (n < 0) {
       console.log('Failed to set the vertex information');
@@ -54,45 +44,53 @@ window.onload = function () {
     }
   
     // Specify the color for clearing <canvas>
+    // 指定用于清除<canvas>的颜色
     gl.clearColor(0, 0, 0, 1);
   
     // Clear <canvas>
+    // 清除canvas
     gl.clear(gl.COLOR_BUFFER_BIT);
   
-    // Draw three points
+    // Draw three points  绘画成点
     gl.drawArrays(gl.POINTS, 0, n);
   }
   
   function initVertexBuffers(gl) {
     var vertices = new Float32Array([
-      0, 0.5,   -0.5, -0.5,   0.5, -0.5
+      0, 0.5,   
+      -0.5, -0.5,  
+      0.5, -0.5
     ]);
-    var n = 3; // The number of vertices
+    var n = 3; // The number of vertices 顶点的数量
   
-    // Create a buffer object
+    // Create a buffer object //创建一个buffer对象
     var vertexBuffer = gl.createBuffer();  
     if (!vertexBuffer) {
       console.log('Failed to create the buffer object');
       return -1;
     }
   
-    // Bind the vertex buffer
+    // Bind the vertex buffer //绑定顶点缓冲区
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    // Write date into the buffer object
+    // Write date into the buffer object //写入日期到缓冲区对象
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
   
     // Assign the buffer object to the attribute variable
+    //将缓冲区对象赋值给属性变量
     var a_Position = gl.getAttribLocation(program, 'a_Position');
     if (a_Position < 0) {
       console.log('Failed to get the storage location of a_Position');
       return -1;
     }
+    // buffer 数据写入显卡
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
   
     // Unbind the buffer object
+    //取消绑定buffer对象
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
    
     // Enable the assignment to a_Position variable
+    //启用a_Position变量赋值
     gl.enableVertexAttribArray(a_Position);
   
     return n;
